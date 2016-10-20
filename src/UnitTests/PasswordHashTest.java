819e95d0271c48375dbf60ca3525b87718f6f26d
Sindhukumar <ssundar6@gmu.edu>
2016-10-20 15:49:09 -0400

    Packages set

---
+++ src/UnitTests/PasswordHashTest.java
@@ -0,0 +1,24 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Tools.HashPassword;
+
+public class PasswordHashTest {
+
+	@Test
+	public void test_passwordHash() {
+		String hash = HashPassword.Hash("dummy@gmail.com","password");
+		System.out.println(hash);
+		
+		//assertTrue(hash == "feea72dc67a25559cc6044e845f298f6758b1007c7f28a4827d0efedb2902458") ;
+		
+		String hash2 = HashPassword.Hash("dummy@gmail.com","feea72dc67a25559cc6044e845f298f6758b1007c7f28a4827d0efedb2902458");
+		System.out.println(hash2);
+		
+		//assertTrue(hash == "feea72dc67a25559cc6044e845f298f6758b1007c7f28a4827d0efedb2902458") ;
+	}
+
+}
